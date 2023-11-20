5e67144f7f93ca82c6dd39394384df9a942f87a2
Veera Hiltunen <veera.hiltunen@gmail.com>
2023-11-20 06:00:18 +0200

    Get all notes and print to console with Note.find method

--- backend/mongo.js
+++ backend/mongo.js
@@ -19,12 +19,19 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-    content: 'HTML is Easy',
-    important: true,
-})
+// const note = new Note({
+//     content: 'HTML is Easy',
+//     important: true,
+// })
 
-note.save().then(result => {
-    console.log('note saved!')
+// note.save().then(result => {
+//     console.log('note saved!')
+//     mongoose.connection.close()
+// })
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
     mongoose.connection.close()
-})
+  })
