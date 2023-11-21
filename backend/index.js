fc59a94f369b0465bf0a9e9691e410a929c102c8
Veera Hiltunen <veera.hiltunen@gmail.com>
2023-11-21 16:01:09 +0200

    Fix order of requirements and middlewares, fix formatting

--- backend/index.js
+++ backend/index.js
@@ -1,80 +1,99 @@
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const express = require('express')
+require("dotenv").config();
 
-const app = express()
+const Note = require("./models/note");
 
-app.use(express.static('dist'))
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
-const cors = require('cors')
-app.use(cors())
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
 
-app.use(express.json())
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
 
-const Note = require('./models/note')
+  next(error);
+};
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+app.use(express.static("dist"));
 
-app.get('/api/notes', (req, res) => {
-  Note.find({}).then(notes => {
-    res.json(notes)
-  })
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+app.get("/api/notes", (req, res) => {
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
+});
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
   if (body.content === undefined) {
-    return response.status(400).json({error: 'content missing'})
+    return response.status(400).json({ error: "content missing" });
   }
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
-  })
+    important: body.important || false,
+  });
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
+
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
     })
+    .catch((error) => next(error));
+});
 
-})
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  return response.status(400).json({
+    error: "note not found",
+  });
+});
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-  return response.status(400).json({ 
-    error: 'note not found' 
-  })})
-
-app.put('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  if (!notes.find(note => note.id === id))
-  {
-    response.status(404).end()
+app.put("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  if (!notes.find((note) => note.id === id)) {
+    response.status(404).end();
   }
-  const newNote = request.body
-  notes = notes.map(note => note.id === id ? newNote : note)
-  response.json(newNote)
-})
+  const newNote = request.body;
+  notes = notes.map((note) => (note.id === id ? newNote : note));
+  response.json(newNote);
+});
 
-const PORT = process.env.PORT
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
