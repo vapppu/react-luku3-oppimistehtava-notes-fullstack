7870443544ac8dceee461887896cf285ed20469f
Veera Hiltunen <veera.hiltunen@gmail.com>
2023-12-01 09:50:22 +0200

    Reorder functions in notes controller to match it with model solution

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -6,16 +6,6 @@ notesRouter.get('/', async (request, response) => {
     response.json(notes)
 })
 
-
-notesRouter.get('/:id', async (request, response) => {
-    const note = await Note.findById(request.params.id)
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).json(note)
-    }
-})
-
 notesRouter.post('/', async (request, response) => {
     const body = request.body
 
@@ -26,6 +16,15 @@ notesRouter.post('/', async (request, response) => {
 
     const savedNote = await note.save()
     response.status(201).json(savedNote)
+})
+
+notesRouter.get('/:id', async (request, response) => {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).json(note)
+    }
 })
 
 notesRouter.delete('/:id', async (request, response) => {
