813355a97500ae05aa88e72d4a0d3ac7d6136f6d
Veera Hiltunen <veera.hiltunen@gmail.com>
2024-01-09 10:00:49 +0200

    Add JSON Web Token verification for login and posting new notes

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,6 +1,7 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => {
     const notes = await Note
@@ -9,10 +10,22 @@ notesRouter.get('/', async (request, response) => {
     response.json(notes)
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
 notesRouter.post('/', async (request, response) => {
     const body = request.body
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
-    const user = await User.findById(body.userId)
 
     const note = new Note({
         content: body.content,
