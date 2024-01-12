bf35606ce5e72dbdb90dd3504ea48236cde82c73
Veera Hiltunen <veera.hiltunen@gmail.com>
2024-01-12 15:26:01 +0200

    Hide note form when submitting a new note with reference to togglable component's togglevisiblity function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
 import Togglable from './components/Togglable'
 import LoginForm from './components/LoginForm'
+import NoteForm from './components/NoteForm'
 import noteService from './services/notes'
 import loginService from './services/login'
 
 const App = () => {
-  const [loginVisible, setLoginVisible] = useState(false)
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+
+  const noteFormRef = useRef()
 
   useEffect(() => {
     noteService
@@ -34,18 +35,15 @@ const App = () => {
     }
   }, [])
 
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() > 0.5,
-    }
+  const addNote = (noteObject) => {
+
+    noteFormRef.current.toggleVisibility()
 
     noteService
       .create(noteObject)
       .then(returnedNote => {
+        console.log(returnedNote)
         setNotes(notes.concat(returnedNote))
-        setNewNote('')
       })
   }
 
@@ -66,10 +64,6 @@ const App = () => {
           setErrorMessage(null)
         }, 5000)
       })
-  }
-
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value)
   }
 
   const notesToShow = showAll
@@ -135,7 +129,9 @@ const App = () => {
       {user &&
         <div>
           <p>{user.name} logged in</p>
-          {noteForm()}
+          <Togglable buttonLabel="new note" ref={noteFormRef}>
+            <NoteForm createNote={addNote} />
+          </Togglable>
         </div>
       }
 
